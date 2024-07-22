939d600ebfea63467b919e0a08275ebfba3cc0dd
Dean Quinney <slpixe@gmail.com>
2024-07-22 08:16:50 +0100

    starting point

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import express, {Express} from 'express';
+import express, { Express, Request, Response } from "express";
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+    // res.send("Express + TypeScript Server");
+    res.status(200).send('hmmm')
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
