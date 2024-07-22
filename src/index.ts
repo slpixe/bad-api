114a16d7c505a164a7a34b9cea8c8d679ac8ff56
Dean Quinney <slpixe@gmail.com>
2024-07-22 08:19:21 +0100

    reqs

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,16 @@ app.get("/", (req: Request, res: Response) => {
     res.status(200).send('hmmm')
 });
 
+app.get("/bad-client", (req: Request, res: Response) => {
+    // res.send("Express + TypeScript Server");
+    res.status(400).send('no')
+});
+
+app.get("/bad-server", (req: Request, res: Response) => {
+    // res.send("Express + TypeScript Server");
+    res.status(500).send('no')
+});
+
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
