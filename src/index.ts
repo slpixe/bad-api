9fba86abe522f5880283ed5286ec839c970ec04b
Dean Quinney <slpixe@gmail.com>
2024-07-22 08:03:45 +0100

    a bit strange

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello');
+import express, {Express} from 'express';
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+});
