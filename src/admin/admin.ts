eabc8322223beaea3be28c3fb84397efbfa054c0
Dean Quinney <slpixe@gmail.com>
2024-08-18 15:05:49 +0100

    admin static page

--- src/admin/admin.ts
+++ src/admin/admin.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Create __dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // const createAdminRouter = ({ config }) => {
 export const createAdminRouter = () => {
     const adminRouter = express.Router();
 
-    adminRouter.use('/', express.static('admin-page'))
+    adminRouter.use('/', express.static(path.join(__dirname, '../admin-page')));
 
     adminRouter.get('/aaa', (req, res) => {
         res.status(200).send('Admin!');
