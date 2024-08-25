5fb5b4646e345d8d51d704830634f4671a76078d
Dean Quinney <slpixe@gmail.com>
2024-08-25 12:03:26 +0100

    biome format middleware

--- src/middleware/networkDelay.ts
+++ src/middleware/networkDelay.ts
@@ -1,13 +1,13 @@
-import { NextFunction, Request, Response } from "express";
-import {settingsStore} from "../settings/settings.js";
+import type { NextFunction, Request, Response } from "express";
+import { settingsStore } from "../settings/settings.js";
 
 export const networkDelayMiddleware = () => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const delayMS = settingsStore.getSettings().networkDelay;
-        console.log('=networkDelayMiddleware - start', delayMS);
-        setTimeout(() => {
-            console.log('=networkDelayMiddleware - finished', delayMS);
-            next();
-        }, delayMS);
-    };
+	return (req: Request, res: Response, next: NextFunction) => {
+		const delayMS = settingsStore.getSettings().networkDelay;
+		console.log("=networkDelayMiddleware - start", delayMS);
+		setTimeout(() => {
+			console.log("=networkDelayMiddleware - finished", delayMS);
+			next();
+		}, delayMS);
+	};
 };
