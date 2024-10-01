32b62f476c048606ec7afdf20e9784fc44c9acf0
Dean Quinney <slpixe@gmail.com>
2024-10-01 21:49:22 +0200

    basic bootstrap

--- public/home-page/home-page.js
+++ public/home-page/home-page.js
@@ -1,160 +1,86 @@
-// public/config-page/config-page.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Button states
+    const getBtn = document.getElementById("get-btn");
+    const postBtn = document.getElementById("post-btn");
+    const putBtn = document.getElementById("put-btn");
+    const sendBtn = document.getElementById("send-btn");
 
-// Debounce function
-function debounce(func, delay) {
-    let timeout;
-    return function (...args) {
-        const context = this;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(context, args), delay);
-    };
-}
+    const notLoadingBtn = document.getElementById("not-loading-btn");
+    const loadingBtn = document.getElementById("loading-btn");
+    const loadedBtn = document.getElementById("loaded-btn");
 
-function formatMillisecondsToSeconds(ms) {
-    return (ms / 1000).toFixed(2)
-}
+    const unknownOkBtn = document.getElementById("unknown-ok-btn");
+    const notOkBtn = document.getElementById("not-ok-btn");
+    const okBtn = document.getElementById("ok-btn");
 
-function formatToPercentage(value) {
-    return (value * 100).toFixed(0) + '%';
-}
+    const statusCodeElem = document.getElementById("status-code");
+    const responseTextElem = document.getElementById("response-text");
 
-// // Example of updating the status indicator
-// function updateStatusIndicator(id, isSuccess) {
-//     const statusEl = document.getElementById(`${id}Status`);
-//     if (statusEl) {
-//         console.log('=aaa', id, isSuccess);
-//         statusEl.textContent = isSuccess ? '✔️' : '❌';
-//     }
-// }
+    // Change API request method button states
+    getBtn.addEventListener("click", function() {
+        resetMethodButtons();
+        getBtn.classList.add("btn-primary");
+    });
 
-// Check client and server values and update status indicators
-function checkAndDisplayStatus() {
-    for (const element of elements) {
-        const clientValue = state[`${element.id}Client`];
-        const serverValue = state[`${element.id}Server`];
-        const statusEl = document.getElementById(`${element.id}Status`);
+    postBtn.addEventListener("click", function() {
+        resetMethodButtons();
+        postBtn.classList.add("btn-primary");
+    });
 
-        if (statusEl) {
-            if (clientValue !== null && clientValue === serverValue) {
-                statusEl.textContent = '✔️️'; // Values match
-            } else {
-                statusEl.textContent = '❌'; // Values do not match
-            }
-        }
+    putBtn.addEventListener("click", function() {
+        resetMethodButtons();
+        putBtn.classList.add("btn-primary");
+    });
 
-        if (element.id === 'networkDelay') {
-            document.getElementById('networkDelayValue').textContent = formatMillisecondsToSeconds(state[`${element.id}Client`]);
-        }
-
-        if (element.id === 'networkDelayChance') {
-            document.getElementById('networkDelayChanceValue').textContent = formatToPercentage(state[`${element.id}Client`]);
-        }
-    }
-}
-
-const socket = io();
-
-/*
-Will be like the following during update:
-// const state = {
-//     networkDelayClient: 1200,
-//     networkDelayServer: 500,
- */
-const state = {};
-
-// Elements to sync
-const elements = [
-    {id: 'networkDelay', event: 'input', type: 'range'},
-    {id: 'networkDelayChance', event: 'input', type: 'range'},
-    {id: 'checkbox1', event: 'change', type: 'checkbox'},
-    {id: 'textInput1', event: 'input', type: 'text'}
-];
-
-// // Function to sync element value with server
-// function syncElement(element) {
-//     const el = document.getElementById(element.id);
-//
-//     const emitChange = debounce(() => {
-//         const value = element.type === 'checkbox' ? el.checked : el.value;
-//         state[`${element.id}Client`] = value; // Save the client-side value
-//         // socket.emit('elementChanged', { id: element.id, value });
-//         socket.emit('configChanged', { id: element.id, value });
-//     }, 300); // Adjust the delay as necessary
-//
-//     el.addEventListener(element.event, emitChange);
-// }
-
-// // Initialize all elements to sync
-// elements.forEach(element => {
-//     syncElement(element);
-// });
-
-socket.on('connect', () => {
-    console.log('Connected to WebSocket server');
-});
-
-socket.on('message', (data) => {
-    console.log('Message from server:', data);
-});
-
-// Send a message to the server
-socket.emit('message', 'Hello from client');
-
-// Listen for updates from server
-socket.on('configSync', (updatedConfig) => {
-    // Update the state with server values
-    console.log('=configSync', updatedConfig);
-
-    for (const [id, value] of Object.entries(updatedConfig)) {
-        state[`${id}Client`] = value;
-        state[`${id}Server`] = value;
-
-        // Update the UI elements with server values
-        const el = document.getElementById(id);
-        if (el) {
-            if (el.type === 'checkbox') {
-                el.checked = value;
-            } else {
-                el.value = value;
-                // if (id === 'networkDelay') {
-                //     document.getElementById('networkDelayValue').textContent = value;
-                // }
-            }
-        }
+    function resetMethodButtons() {
+        getBtn.classList.remove("btn-primary");
+        postBtn.classList.remove("btn-primary");
+        putBtn.classList.remove("btn-primary");
     }
 
-    // Check if client and server values match, and update status indicators
-    checkAndDisplayStatus();
-});
+    // Change Response status buttons
+    notLoadingBtn.addEventListener("click", function() {
+        resetStatusButtons();
+        notLoadingBtn.classList.add("btn-secondary");
+    });
 
-// Function to sync element value with server
-function initElements(element) {
-    console.log('=initElements', element);
-    const el = document.getElementById(element.id);
+    loadingBtn.addEventListener("click", function() {
+        resetStatusButtons();
+        loadingBtn.classList.add("btn-secondary");
+    });
 
-    const emitChange = debounce(() => {
-        console.log('=emitChange');
-        console.log('=old client state', state);
-        const value = element.type === 'checkbox' ? el.checked : el.value;
-        state[`${element.id}Client`] = value; // Update client-side value in state
-        console.log('=new client state', state);
-        checkAndDisplayStatus();
+    loadedBtn.addEventListener("click", function() {
+        resetStatusButtons();
+        loadedBtn.classList.add("btn-primary");
+        statusCodeElem.textContent = "Status Code: 200";
+    });
 
-        // Emit all the updated client-side config to the server
-        const clientConfig = {};
-        for (const key in state) {
-            if (key.endsWith('Client')) {
-                const id = key.replace('Client', '');
-                clientConfig[id] = state[key];
-            }
-        }
-        socket.emit('configSync', clientConfig);
-    }, 300); // Adjust the delay as necessary
+    function resetStatusButtons() {
+        notLoadingBtn.classList.remove("btn-primary", "btn-secondary");
+        loadingBtn.classList.remove("btn-primary", "btn-secondary");
+        loadedBtn.classList.remove("btn-primary", "btn-secondary");
+    }
 
-    el.addEventListener(element.event, emitChange);
-}
+    // OK buttons for responses
+    okBtn.addEventListener("click", function() {
+        resetResponseButtons();
+        okBtn.classList.add("btn-primary");
+        responseTextElem.textContent = '{"a": "b"}';
+    });
 
-// Initialize all elements to sync
-elements.forEach(element => {
-    initElements(element);
+    unknownOkBtn.addEventListener("click", function() {
+        resetResponseButtons();
+        unknownOkBtn.classList.add("btn-secondary");
+    });
+
+    notOkBtn.addEventListener("click", function() {
+        resetResponseButtons();
+        notOkBtn.classList.add("btn-secondary");
+    });
+
+    function resetResponseButtons() {
+        okBtn.classList.remove("btn-primary");
+        unknownOkBtn.classList.remove("btn-primary", "btn-secondary");
+        notOkBtn.classList.remove("btn-primary", "btn-secondary");
+    }
 });
